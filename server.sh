fac93f5f111b5380c35f78314b0246cf41226b79
Sherlock297 <84141268+Sherlock297@users.noreply.github.com>
2021-05-13 20:37:22 +0530

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+printf "\n\n"
+
+echo -e "\e[30;48;5;82mPLEASE WAIT SCRITI IS RUNNING........!It will take some time depending on the domains provided by you.>
+
+printf "\n\n"
+
+echo -e "To see VERBOSE output please run the command as './server.sh -v'"
+
+for urls in  $(cat domains.txt);
+        do echo -e "\e[38;5;47m $urls\e[0m"
+                 curl -k $1 -s -I $urls | grep 'Server:\|X-Powered-By:'
+done > server_output.txt
+
+printf "\n\n"
+
+echo -e "\e[30;48;5;82mDone...!!!\e[0m"
+
+printf "\n\n"
+
+echo -e "\e[30;48;5;82mServer and X-Powered-By Versions are as follows : - \e[0m"
+
+printf "\n\n"
+
+cat server_output.txt
+
+printf "\n\n"
